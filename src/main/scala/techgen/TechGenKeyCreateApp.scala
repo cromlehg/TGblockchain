bc20db52165cd6490678d926120cfe8896534dad
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2021-06-09 12:54:05 +0300

    refactor TechGenKeysCreateApp

--- src/main/scala/techgen/TechGenKeyCreateApp.scala
+++ src/main/scala/techgen/TechGenKeyCreateApp.scala
@@ -1,22 +1,18 @@
 package techgen
 
-import techgen.blocks.BDBlock
-import scorex.core.transaction.state.PrivateKey25519Companion
-import scorex.util.{ScorexEncoding, ScorexLogging}
-import scorex.core.transaction.state.PrivateKey25519Companion
-import scorex.core.transaction.state.PrivateKey25519
-import scorex.core.transaction.state.PrivateKey25519Serializer
-import scorex.core.transaction.box.proposition.{ProofOfKnowledgeProposition, PublicKey25519Proposition}
+import scorex.core.transaction.box.proposition.PublicKey25519Proposition
+import scorex.core.transaction.state.{PrivateKey25519, PrivateKey25519Companion}
+import scorex.util.ScorexEncoding
 
 object TechGenKeysCreateApp extends App with ScorexEncoding {
 
   val (priv: PrivateKey25519, pub: PublicKey25519Proposition) =
      PrivateKey25519Companion.generateKeys("genesis".getBytes)
-     
-   println("Keys: ")
-   println("PRIVATE: ")
-   println(encoder.encode(priv.privKeyBytes))
-   println("PUBLIC: ")
-   println(pub.toString())
+
+  println("Keys: ")
+  println("PRIVATE: ")
+  println(encoder.encode(priv.privKeyBytes))
+  println("PUBLIC: ")
+  println(pub.toString())
 
 }
